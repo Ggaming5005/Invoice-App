6a11cba4400da21236fb9f077493e429cc63e20c
Ggaming5005 <ggaming5005@gmail.com>
2025-05-09 09:04:19 -0700

    1

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useContext } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import "normalize.css";
+import { AnimatePresence } from "framer-motion";
+
+import GlobalStyle from "./styles/globalStyles";
+import Header from "./components/Header/Header";
+import Home from "./pages/Home";
+import InvoiceDetails from "./pages/InvoiceDetails";
+import Drawer from "./components/Drawer";
+import InvoiceFormContainer from "./components/InvoiceForm/InvoiceFormContainer";
+
+import { lightTheme, darkTheme } from "./styles/theme";
+
+import { AppContext } from "./context/AppContext";
+
+function App() {
+  const location = useLocation();
+  const { theme, isDrawerOpen } = useContext(AppContext);
+
+  return (
+    <div className="App">
+      <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+        <GlobalStyle isDrawerOpen={isDrawerOpen} />
+        <Header />
+        <AnimatePresence exitBeforeEnter>
+          <Routes location={location} key={location.pathname}>
+            <Route path="/" element={<Home />} />
+            <Route path="/:invoiceId" element={<InvoiceDetails />} />
+          </Routes>
+        </AnimatePresence>
+        <Drawer isOpen={isDrawerOpen}>
+          <InvoiceFormContainer />
+        </Drawer>
+      </ThemeProvider>
+    </div>
+  );
+}
+//luka is a nerd 🤓
+export default App;
